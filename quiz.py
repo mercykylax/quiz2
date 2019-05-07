03201e55bc0663863972ca63be11644db72e9420
Mercykylax <nyawiramercy49@gmail.com>
2019-05-07 19:29:34 +0300

    Test 2

---
+++ quiz.py
@@ -0,0 +1,53 @@
+def tens
+   a=(1,2,3,4,5,6,7,8,9)
+    c=list(a*10)  
+    for x in a
+    print (x)
+
+
+    
+for i in list (2,4,6,8,10,12)
+print(x)
+
+def squares
+a= [] squares in list (2,4,6,8,10,12):
+squares.append(x**2)
+
+
+la=[9,1,4,7,3]
+lb=[5,2,6,8,0]
+la.extend(lb)
+sorted(la)
+
+
+def k(p):  
+ for p in range(0,f(n-1)):
+ return p*y(n) + p*f(n-1) + p*f(n)
+
+def largest
+largest([1,3,6,8,2,4,5.7])
+print(max(largest)) 
+
+def smallest
+smallest([1,3,6,8,2,4,5.7])
+print(min(smallest))
+
+def my_modulus
+    for i in range (10,20)
+    
+
+
+
+
+
+ students =[
+    {'balance': 1000, 'name': 'Irene'} 
+    {'balance': 2000, 'name': 'Pauline'}
+    {'balance': 3000, 'name': 'Naima'} 
+    {'balance': 1000, 'name': 'Rose'}]
+
+print (“hello {}, your balance is {}”.format(name,balance) )
+
+
+
+
